a73cf15ba7f37088689dc58a3cac321ff4797b8b
rjkaneumass <151099889+rjkaneumass@users.noreply.github.com>
2024-04-01 20:47:10 -0400

    Update script.js

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,1 +1,20 @@
+function wordHighlighter(){
+    let word1 = document.getElementById('keyword').value;
+    let all_items = document.getElementsByTagName("section");
+    let x = all_items.length();
+    let current_item = all_items.item(0);
+    let current_HTML = "";
+    let new_HTML = "";
+    for(let i = 0; i < x; i++){
+        current_item = all_items.item(0);
+        current_HTML = current_item.innerHTML;
+        if(current_HTML.indexOf(word1) != -1){
+            new_HTML += current_HTML.substring(0, current_HTML.indexOf(word1));
+            new_HTML += "<span class=\"highlight\">" + word1 + "</span>";
+            current_HTML = current_HTML.substring(current_HTML.indexOf(word1) + word1.length);
+        }
+        current_item.innerHTML = new_HTML;
+    }
+}
 
+document.getElementById("button").addEventListener("click", wordHighlighter());
