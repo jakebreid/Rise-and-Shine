c3ed0687fecdf9fbd5a64d57ffb86cefd1379f98
Daniel Friedman <daniel.eric.friedman@verizon.net>
2024-04-01 22:16:49 -0400

    script changes

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,17 +1,24 @@
-function wordHighlighter(){
+function wordHighlighter() {
     let word1 = document.getElementById('keyword').value;
     let all_items = document.getElementsByTagName("section");
     let x = all_items.length();
     let current_item = all_items.item(0);
+    
     let current_HTML = "";
     let new_HTML = "";
+    let index = -1;
+    
     for(let i = 0; i < x; i++){
-        current_item = all_items.item(0);
-        current_HTML = current_item.innerHTML;
-        if(current_HTML.indexOf(word1) != -1){
-            new_HTML += current_HTML.substring(0, current_HTML.indexOf(word1));
-            new_HTML += "<span class=\"highlight\">" + word1 + "</span>";
-            current_HTML = current_HTML.substring(current_HTML.indexOf(word1) + word1.length);
+        current_item = all_items.item(x); //Get current section
+        current_HTML = current_item.innerHTML; //Get HTML of current section
+        index = current_HTML.toLowercase().indexOf(word1.toLowercase());
+        while(index !== -1){ //while index of input is found
+            new_HTML += current_HTML.substring(0, index);
+            new_HTML += '<span class="highlight">' + word1 + '</span>';
+            new_HTML += current_HTML.substring(index + word1.length);
+            //current_HTML = current_HTML.substring(current_HTML.indexOf(word1) + word1.length);
+         
+            index = current_HTML.toLowercase().indexOf(word1.toLowercase(), index + 1); // find next index of word.
         }
         current_item.innerHTML = new_HTML;
     }
