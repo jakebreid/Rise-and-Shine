a823f64d58beef66c26756f1bd1a6bc53464a3fc
Jake Reid <jbreid@umass.edu>
2024-04-18 16:35:58 -0400

    git push -u origin main

---
+++ src/client/main.js
@@ -0,0 +1,22 @@
+//JAKE WORKSPACE HERE 
+
+import PouchDB from "pouchdb";
+const db = new PouchDB("name");
+const userName = "";
+if (db.get("username"))
+{
+
+}
+
+
+//DAN WORKSPACE HERE 
+
+
+
+
+//LINDSEY WORKSPACE HERE 
+
+
+
+
+//ROBERT WORKSPACE HERE
