7343fd6a75abc4fa899280ab9aca938e2e94d140
Jake Reid <jbreid@umass.edu>
2024-04-19 11:24:58 -0400

    git push -u origin main

--- src/client/script.js
+++ src/client/script.js
@@ -2,14 +2,11 @@
 
 import PouchDB from "pouchdb";
 const db = new PouchDB("myName");
-const userName = "";
-if (!(await db.get("username")))
-{
-    prompt("Greetings! We recognize this is your first time here!\nPlease Enter Your Name:", "");
+const userName = await db.get("username");
+if (typeof userName == "boolean")
+{ 
+userName = prompt("Greetings! We recognize this is your first time here!\nPlease Enter Your Name:", "");
 }
-prompt("hey bitch", "");
-alert("boo");
-
 
 //DAN WORKSPACE HERE 
 
