a4950dbdad6573931bd54f3bb7d49eb7f2c76404
Jake Reid <jbreid@umass.edu>
2024-04-19 11:37:10 -0400

    git push -u origin main

--- src/client/script.js
+++ src/client/script.js
@@ -1,12 +1,13 @@
 //JAKE WORKSPACE HERE 
 
-import PouchDB from "pouchdb";
-const db = new PouchDB("myName");
-const userName = await db.get("username");
-if (typeof userName == "boolean")
-{ 
-userName = prompt("Greetings! We recognize this is your first time here!\nPlease Enter Your Name:", "");
-}
+//import PouchDB from "pouchdb";
+//const db = new PouchDB("myName");
+//const userName = await db.get("username");
+//if (typeof userName == "boolean")
+//{ 
+const userName = prompt("Greetings! We recognize this is your first time here!\nPlease Enter Your Name:", "");
+//}
+document.getElementById("myTitle").innerHTML = "Rise and Shine, " + userName + "!";
 
 //DAN WORKSPACE HERE 
 
